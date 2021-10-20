dbc63d5743f651ff851d9695363479d6e61c54c1
pedro <pfreitas@id.uff.br>
2021-10-20 16:12:34 -0300

    add project struct

---
+++ app/src/main/java/com/freitaspedro/countries/model/Data.kt
@@ -0,0 +1,3 @@
+package com.freitaspedro.countries.model
+
+data class Country(val name: String?)
---
+++ app/src/main/java/com/freitaspedro/countries/viewmodel/ListViewModel.kt
@@ -0,0 +1,6 @@
+package com.freitaspedro.countries.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class ListViewModel: ViewModel() {
+}
