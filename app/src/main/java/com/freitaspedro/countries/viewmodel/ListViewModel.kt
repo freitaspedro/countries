4bb44ee96e9278691eb27ebc7cb5e1b0fad0b4f9
pedro <pfreitas@id.uff.br>
2021-10-20 17:06:50 -0300

    add view model sketch

--- app/src/main/java/com/freitaspedro/countries/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/freitaspedro/countries/viewmodel/ListViewModel.kt
@@ -1,6 +1,36 @@
 package com.freitaspedro.countries.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.freitaspedro.countries.model.Country
 
 class ListViewModel: ViewModel() {
+
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData = listOf(
+            Country("Country A"),
+            Country("Country B"),
+            Country("Country C"),
+            Country("Country D"),
+            Country("Country E"),
+            Country("Country F"),
+            Country("Country G"),
+            Country("Country H"),
+            Country("Country I"),
+            Country("Country J"),
+        )
+
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
+
 }
