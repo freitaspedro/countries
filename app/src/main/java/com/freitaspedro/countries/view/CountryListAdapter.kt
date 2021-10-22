757705da1635017eb5978b0588519fa2a91f3d9c
pedro <pfreitas@id.uff.br>
2021-10-22 15:41:30 -0300

    add list adapter

---
+++ app/src/main/java/com/freitaspedro/countries/view/CountryListAdapter.kt
@@ -0,0 +1,39 @@
+package com.freitaspedro.countries.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.freitaspedro.countries.R
+import com.freitaspedro.countries.model.Country
+
+class CountryListAdapter(var countries: ArrayList<Country>): RecyclerView.Adapter<CountryListAdapter.CountryViewHolder>() {
+
+    fun updateCountries(newCountries: List<Country>) {
+        countries.clear()
+        countries.addAll(newCountries)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int) = CountryViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_country, parent, false)
+    )
+
+    override fun getItemCount() = countries.size
+
+    override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+        holder.bind(countries[position])
+    }
+
+    class CountryViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+        private val countryName = view.findViewById<TextView>(R.id.nameTextView)
+
+        fun bind(country: Country) {
+            countryName.text = country.name
+        }
+
+    }
+
+}
